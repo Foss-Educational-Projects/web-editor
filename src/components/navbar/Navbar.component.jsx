5ddb16af72b00552730a105d4ece9f311213b062
Foster Z <fosterunlimited0123@gmail.com>
2025-02-16 22:29:19 +0530

    feat: add button & icon to the navbar and set the layout

--- src/components/navbar/Navbar.component.jsx
+++ src/components/navbar/Navbar.component.jsx
@@ -1,8 +1,15 @@
+import { Button } from 'antd';
+
+import { LayoutOutlined } from '@ant-design/icons'
+
 import './navbar.component.scss';
 
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <nav className='navbar__main'>
+            <Button size='large' type='primary'><LayoutOutlined style={{fontSize: '24px'}} /></Button>
+            <h1 className='navbar__title'>Web Language Converter</h1>
+        </nav>
     )
 }
 export default Navbar;
