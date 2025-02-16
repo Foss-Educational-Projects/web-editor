7ea6e7d803aa0c9943191ba8b6ab92c4807b090d
Foster Z <fosterunlimited0123@gmail.com>
2025-02-16 22:29:50 +0530

    feat: add global styling & import sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './App.scss'
 import Navbar from './components/navbar/Navbar.component'
+import Sidebar from './components/sidebar/Sidebar.component';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<Sidebar />
 		</div>
 	)
 }
