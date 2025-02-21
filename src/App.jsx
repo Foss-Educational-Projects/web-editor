19b9a8a9d21732bef1e5f7d15f3cb3a127cf5389
Foster Z <fosterunlimited0123@gmail.com>
2025-02-21 21:51:59 +0530

    fix: add context provider for global access

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './App.scss'
 import Navbar from './components/navbar/Navbar.component'
 import Sidebar from './components/sidebar/Sidebar.component';
+import Playground from './components/playground/Playground.cpmponent'
+
+import AppContext from './context/App.context';
 
 function App() {
+	const [show, setShow] = useState(false)
 	return (
-		<div className="App">
-			<Navbar />
-			<Sidebar />
-		</div>
+		<AppContext.Provider value={{ show, setShow }}>
+			<div className="App">
+				<Navbar />
+				<Sidebar />
+				<Playground />
+			</div>
+		</AppContext.Provider>
 	)
 }
 export default App
