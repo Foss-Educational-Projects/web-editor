9bece1876415bd101c8b05df2a698ae204b29c90
Foster Z <fosterunlimited0123@gmail.com>
2025-02-21 21:23:51 +0530

    fix: add positioning to sidebar component:

--- src/components/sidebar/SIdebar.component.jsx
+++ src/components/sidebar/SIdebar.component.jsx
@@ -1,8 +1,35 @@
+import SidebarToggleButton from '../../modules/SidebarToggleButton.module';
+
 import './sidebar.component.scss';
+import { useContext } from 'react';
+
+import AppContext from '../../context/App.context';
+
 
 const Sidebar = () => {
+    const { show } = useContext(AppContext)
+    const sidebarList = [
+        { id: 1, title: 'Markdown Converter', url: '' },
+        { id: 2, title: 'SCSSConverter', url: '' },
+        { id: 3, title: 'Typescript Converter', url: '' },
+    ]
     return (
-        <h1>Sidebar</h1>
+        <aside className='sidebar__main' style={{ left: show ? '5px' : '-300px' }}>
+            <header className='sidebar__header__title'>
+                <h3>Converters</h3>
+                <SidebarToggleButton />
+            </header>
+            <ul className='sidebar__list'>
+                {sidebarList.map((item) => {
+                    return (
+                        <li key={item.id} className='sidebar__list__item'>
+                            <a href='#'>{item.title}</a>
+                        </li>
+                    )
+                })}
+
+            </ul>
+        </aside>
     )
 }
 export default Sidebar;
