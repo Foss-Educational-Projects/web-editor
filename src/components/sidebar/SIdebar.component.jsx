2174d04b1d6cf1f067ae29d65aea7007ff54ac28
Foster Z <fosterunlimited0123@gmail.com>
2025-03-01 23:41:54 +0530

    feat(Editor): add editor control settings

--- src/components/sidebar/SIdebar.component.jsx
+++ src/components/sidebar/SIdebar.component.jsx
@@ -1,18 +1,30 @@
-import SidebarToggleButton from '../../modules/SidebarToggleButton.module';
+import { Dropdown } from 'react-bootstrap';
+
+import SidebarToggleButton from '../../modules/sidebar-toggle-button/SidebarToggleButton.module';
 
 import './sidebar.component.scss';
 import { useContext } from 'react';
 
 import AppContext from '../../context/App.context';
 
+import Control from './../control/Control.component'
 
 const Sidebar = () => {
     const { show } = useContext(AppContext)
     const sidebarList = [
         { id: 1, title: 'Markdown Converter', url: '' },
-        { id: 2, title: 'SCSSConverter', url: '' },
+        { id: 2, title: 'SCSS Converter', url: '' },
         { id: 3, title: 'Typescript Converter', url: '' },
     ]
+    const ColorSchemes = [
+        { id: 1, title: 'GitHub Light', value: 'github' },
+        { id: 1, title: 'Monokai', value: 'monokai' },
+        { id: 1, title: 'Twilight', value: 'twilight' }
+    ]
+    // Trigger Handlers
+    const handleSelect = () => {
+        
+    }
     return (
         <aside className='sidebar__main' style={{ left: show ? '5px' : '-300px' }}>
             <header className='sidebar__header__title'>
@@ -29,6 +41,21 @@ const Sidebar = () => {
                 })}
 
             </ul>
+            <Control />
+            <Dropdown>
+                <Dropdown.Toggle style={{ width: '100%', borderRadius: '20px' }} variant="secondary" id="dropdown-basic">
+                    Change Colorscheme
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu variant='dark' style={{ width: '100%' }}>
+                    {ColorSchemes.map((schemes) => {
+                        return (
+                            <Dropdown.Item onSelect={handleSelect(schemes.value)} variant='secondary' key={schemes.id} href="#">{schemes.title}</Dropdown.Item>
+                        )
+                    })}
+                </Dropdown.Menu>
+            </Dropdown>
+            
         </aside>
     )
 }
