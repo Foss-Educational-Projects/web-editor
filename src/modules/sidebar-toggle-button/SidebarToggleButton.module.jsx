252b04a84c00ec4bbf97da7e15921d37523994d7
Foster Z <fosterunlimited0123@gmail.com>
2025-03-01 23:44:34 +0530

    feat(SidebarToggleButton): add sidebar toggle button

---
+++ src/modules/sidebar-toggle-button/SidebarToggleButton.module.jsx
@@ -0,0 +1,23 @@
+// React Imports
+import { useState, useContext } from 'react';
+
+// React Bootstrap Imports
+import { Button } from 'react-bootstrap';
+// React Icons Imports
+import { BsLayoutSidebar } from "react-icons/bs";
+
+// Stylesheet Imports
+import './sidebarToggleButton.module.scss';
+
+// User Context Imports
+import AppContext from '../../context/App.context';
+
+// Root Component (SidebarToggleButton)
+const SidebarToggleButton = () => {
+    const {show, setShow} = useContext(AppContext)
+    return (
+        <Button onClick={() => { setShow(!show) }} variant='secondary' className='align__icon sidebar__button' size='lg'><BsLayoutSidebar /></Button>
+    )
+}
+
+export default SidebarToggleButton;
