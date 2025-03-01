d828547397de1e287709f05422e6bb130115dcaa
Foster Z <fosterunlimited0123@gmail.com>
2025-03-01 23:46:22 +0530

    feat(Editor): add editor component from ace library

--- src/components/editor/Editor.component.jsx
+++ src/components/editor/Editor.component.jsx
@@ -1,9 +1,27 @@
+import AceEditor from 'react-ace'
+
 import './editor.component.scss';
+
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/ext-language_tools";
+import 'ace-builds/src-noconflict/theme-gruvbox_dark_hard';
 
 const Editor = () => {
     return (
         <div className='container editor__main'>
-        {/* <Editor theme="vs-dark" language="javascript" defaultValue="// Write Code..." height="calc(100vh - 65px)" /> */}
+            <AceEditor
+                enableLiveAutocompletion
+                mode="javascript"
+                theme="gruvbox_dark_hard"
+                height='100%'
+                width='100%'
+                // onChange={onChange}
+                name="editor"
+                editorProps={{ $blockScrolling: true }}
+                className='ace__editor__main'
+            />
         </div>
     )
 }
