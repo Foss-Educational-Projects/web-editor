d4142fa45156f62e5afa0eca954cc9580c73d87a
Foster Z <fosterunlimited0123@gmail.com>
2025-03-01 23:43:14 +0530

    fix(nothing): no changes made

--- src/components/navbar/Navbar.component.jsx
+++ src/components/navbar/Navbar.component.jsx
@@ -1,11 +1,13 @@
-import SidebarToggleButton from '../../modules/SidebarToggleButton.module';
+import SidebarToggleButton from '../../modules/sidebar-toggle-button/SidebarToggleButton.module';
+import ThemeToggleButton from '../../modules/theme-toggle-button/ThemeToggleButton.module';
 import './navbar.component.scss';
 
 const Navbar = () => {
     return (
         <nav className='navbar__main'>
             <SidebarToggleButton />
-            <h3 className='navbar__title'>Language Converter</h3>
+            <h3 className='navbar__title'>Web Editor</h3>
+            <ThemeToggleButton />
         </nav>
     )
 }
