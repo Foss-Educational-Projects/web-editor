4e4c6ef786219727ab374abf701872a4307f0936
Foster Z <fosterunlimited0123@gmail.com>
2025-03-01 23:51:41 +0530

    fix(App): no changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './App.scss'
 import Navbar from './components/navbar/Navbar.component'
 import Sidebar from './components/sidebar/Sidebar.component';
-import Playground from './components/playground/Playground.cpmponent'
+import Playground from './components/playground/Playground.component'
 
 import AppContext from './context/App.context';
 
 function App() {
-	const [show, setShow] = useState(false)
+	let title = null;
+	useEffect(() => {
+		document.title = "Web Editor"
+		title = document.title
+		const darkMode = localStorage.getItem('darkmode')
+		console.log(darkMode)
+		if(darkMode) {
+			document.querySelector('html').setAttribute('data-bs-theme', 'dark')
+		}
+		else {
+			document.querySelector('html').setAttribute('data-bs-theme', 'light')
+		}
+		
+	}, [])
+	const [show, setShow] = useState(true)
 	return (
-		<AppContext.Provider value={{ show, setShow }}>
+		<AppContext.Provider value={{ title, show, setShow }}>
 			<div className="App">
 				<Navbar />
 				<Sidebar />
