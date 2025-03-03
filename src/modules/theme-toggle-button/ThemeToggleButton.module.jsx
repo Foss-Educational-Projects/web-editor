46e5e71f232f0487e86eb7cf00c3b2a0931c143f
Foster Z <fosterunlimited0123@gmail.com>
2025-03-03 17:47:08 +0530

    fix(ThemeToggleButton): fix switching of themes between dark & light mode

--- src/modules/theme-toggle-button/ThemeToggleButton.module.jsx
+++ src/modules/theme-toggle-button/ThemeToggleButton.module.jsx
@@ -3,19 +3,42 @@ import './ThemeToggleButton.module.scss';
 import { useState, useEffect } from 'react';
 import { LuMoonStar, LuSun } from "react-icons/lu";
 
+
 const ThemeToggleButton = () => {
-    const [dark, setDark] = useState(true)
-    const handleTheme = () => {
-        setDark(!dark)
-        console.log(dark)
-        if(dark) {
+    const [dark, setDark] = useState(false)
+    let darkMode = localStorage.getItem('darkmode');
+    let systemThemeIsDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+    useEffect(() => {
+        console.log(systemThemeIsDark)
+        if(darkMode === 'true') {
+            setDark(true)
             document.querySelector('html').setAttribute('data-bs-theme', 'dark')
-            localStorage.setItem('darkmode', dark)
+        }
+        else if (!systemThemeIsDark && darkMode == undefined) {
+            document.querySelector('html').setAttribute('data-bs-theme', 'light')
+        }
+        else if (systemThemeIsDark && darkMode == undefined) {
+            document.querySelector('html').setAttribute('data-bs-theme', 'dark')
         }
         else {
             document.querySelector('html').setAttribute('data-bs-theme', 'light')
-            localStorage.setItem('darkmode', !dark)
         }
+    }, [])
+    const handleTheme = () => {
+        console.log("Working")
+        
+        if(dark) {
+            
+            console.log("Light Mode Is True: " + dark)
+            document.querySelector('html').setAttribute('data-bs-theme', 'light')
+            localStorage.setItem('darkmode', false)
+        }
+        else {
+            console.log("Dark Mode Is True: " + dark)
+            document.querySelector('html').setAttribute('data-bs-theme', 'dark')
+            localStorage.setItem('darkmode', true)
+        }
+        setDark(!dark)
     }
     return (
         <div>
