061ebd05b1beef5d8bf9cecdee012a32b76cf959
Foster Z <fosterunlimited0123@gmail.com>
2025-05-31 21:38:04 +0530

    feat: add more properties for editor to the global store

--- src/store/sidebar.store.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand'
-
-const useSidebarStore = create((set) => {
-    toggle: true,
-    toggleSidebar: () => set((state) => toggle)
-})
--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,13 @@ import { create } from 'zustand'
 
 const store = create((set) => ({
     fontSize: 16,
+    tabWidth: 4,
+    autosave: false,
+    useSpace: false,
+    snippets: false,
+    themeing: 'github',
+    language: 'javascript',
+    toggleSnippets: () => set((state) => ({ snippets: !state.snippets})),
+    changeThemes: (theme) => set((state) => ({ themeing: theme }))
 }))
 export { store };
