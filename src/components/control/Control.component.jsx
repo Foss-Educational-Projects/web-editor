d82bfc303fa34be1548b97aef5226aec42c25ca8
Foster Z <fosterunlimited0123@gmail.com>
2025-06-05 17:52:12 +0530

    feat(array): add options for select tag into an array of objects

--- src/components/control/Control.component.jsx
+++ src/components/control/Control.component.jsx
@@ -1,3 +1,7 @@
+// React Imports
+import { useState } from 'react';
+
+
 // React Bootstrap Imports
 import { Form, InputGroup } from 'react-bootstrap'
 import { store } from '../../store/store';
@@ -7,6 +11,28 @@ import './control.component.scss';
 // Root Component (Control)
 const Control = () => {
     const useStore = store((state) => state)
+    const fontFamilyList = [
+        { id: 1, title: 'Ubuntu Mono', value: 'UbuntuMono' },
+        { id: 2, title: 'Iosevka', value: 'Iosevka' },
+        { id: 3, title: 'Cascadia Code', value: 'CascadiaCode' },
+        { id: 4, title: 'Hack', value: 'Hack' },
+        { id: 5, title: 'JetBrains Mono', value: 'JetBrainsMono' },
+        { id: 6, title: 'Fira Code', value: 'FiraCode' },
+        { id: 7, title: 'DejaVu Sans', value: 'DejaVuSans' }
+    ]
+    const fontSizeList = [
+        { id: 1, title: '12px', value: 12 },
+        { id: 2, title: '14px', value: 14 },
+        { id: 3, title: '16px', value: 16 },
+        { id: 4, title: '18px', value: 18 },
+        { id: 5, title: '20px', value: 20 }
+    ]
+    
+    const tabWidthList = [
+        { id: 1, value: 2 },
+        { id: 1, value: 4 },
+        { id: 1, value: 8 }
+    ]
     return (
         <Form action="" className='editor__control__form'>
             <fieldset className='editor__control__form--settings'>
@@ -15,18 +41,31 @@ const Control = () => {
                 <Form.Check className='mb-3' type='checkbox' label='Use Spaces Instead of Tabs' />
                 <InputGroup className='d-flex justify-center align-items-center mb-3'>
                     <Form.Label id='editor-fontsize' className='mx-1 mb-0'>Font Size : </Form.Label>
-                    <Form.Select size='sm' id='editor-fontsize'>
-                        <option className='text-center fw-bolder' value="1">12px</option>
-                        <option className='text-center fw-bolder' value="2">14px</option>
-                        <option className='text-center fw-bolder' value="3">16px</option>
+                    <Form.Select size='sm' id='editor-fontsize' defaultValue={useStore.fontSize} onChange={(e) => useStore.changeFontSize(e.target.value)}>
+                        {fontSizeList.map((item) => {
+                            return (
+                                <option key={item.id} className='text-center fw-bolder' value={item.value}>{item.title}</option>
+                            )
+                        })}
                     </Form.Select>
                 </InputGroup>
                 <InputGroup className='d-flex justify-center align-items-center'>
                     <Form.Label id='editor-tabwidth' className='mx-1 mb-0'>Tab Width : </Form.Label>
                     <Form.Select size='sm' id='editor-tabwidth'>
-                        <option className='text-center fw-bolder' value="2">2</option>
-                        <option className='text-center fw-bolder' value="4">4</option>
-                        <option className='text-center fw-bolder' value="6">6</option>
+                        {tabWidthList.map((item) => {
+                            return (
+                                <option key={item.id} className='text-center fw-bolder' value={item.value}>{item.value}</option>
+                            )
+                        })}
+                    </Form.Select>
+                    <Form.Select style={{marginTop: 5}} size='sm' id='editor-fontfamily'>
+
+                        {fontFamilyList.map((item) => {
+                            return (
+                                <option key={item.id} className='text-center fw-bolder' value={item.value}>{item.title}</option>
+                            )
+                        })}
+                        
                     </Form.Select>
                 </InputGroup>
                 <InputGroup className='mt-3'>
