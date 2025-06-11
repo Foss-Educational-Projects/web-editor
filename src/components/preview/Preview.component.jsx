fccc9feedb58cad3acd11ff64d0cd32a3d17502b
Foster Z <fosterunlimited0123@gmail.com>
2025-06-11 22:01:30 +0530

    feat(toolbar): add the toolbar component to preview component

--- src/components/preview/Preview.component.jsx
+++ src/components/preview/Preview.component.jsx
@@ -1,9 +1,12 @@
+// Custom User Imports
+import Toolbar from '../toolbar/Toolbar.component';
+
 import './preview.component.scss';
 
 const Preview = () => {
     return (
         <div className='container preview__main'>
-            
+            <Toolbar />
         </div>
     )
 }
