3f57f75d6085f6a7abfafac26f8b09d1ec020130
Foster Z <fosterunlimited0123@gmail.com>
2025-06-11 20:49:05 +0530

    feat(event): add respective store functions

--- src/components/control/Control.component.jsx
+++ src/components/control/Control.component.jsx
@@ -1,6 +1,3 @@
-// React Imports
-import { useState } from 'react';
-
 
 // React Bootstrap Imports
 import { Form, InputGroup } from 'react-bootstrap'
@@ -20,6 +17,7 @@ const Control = () => {
         { id: 6, title: 'Fira Code', value: 'FiraCode' },
         { id: 7, title: 'DejaVu Sans', value: 'DejaVuSans' }
     ]
+
     const fontSizeList = [
         { id: 1, title: '12px', value: 12 },
         { id: 2, title: '14px', value: 14 },
@@ -27,21 +25,22 @@ const Control = () => {
         { id: 4, title: '18px', value: 18 },
         { id: 5, title: '20px', value: 20 }
     ]
-    
+
     const tabWidthList = [
         { id: 1, value: 2 },
-        { id: 1, value: 4 },
-        { id: 1, value: 8 }
+        { id: 2, value: 4 },
+        { id: 3, value: 8 }
     ]
+
     return (
         <Form action="" className='editor__control__form'>
             <fieldset className='editor__control__form--settings'>
                 <legend>Editor Settings</legend>
                 <Form.Check checked={useStore.snippets ? true : false} className='mb-3' type='checkbox' onChange={() => useStore.toggleSnippets()} label='Autocompletion' />
-                <Form.Check className='mb-3' type='checkbox' label='Use Spaces Instead of Tabs' />
+                <Form.Check className='mb-3' type='checkbox' label='Use Spaces Instead of Tabs' checked={useStore.useSpace ? true : false} onChange={() => useStore.toggleSpacing()} />
                 <InputGroup className='d-flex justify-center align-items-center mb-3'>
                     <Form.Label id='editor-fontsize' className='mx-1 mb-0'>Font Size : </Form.Label>
-                    <Form.Select size='sm' id='editor-fontsize' defaultValue={useStore.fontSize} onChange={(e) => useStore.changeFontSize(e.target.value)}>
+                    <Form.Select className='editor__control--select text-white' size='sm' id='editor-fontsize' defaultValue={useStore.fontSize} onChange={(e) => useStore.changeFontSize(e.target.value)}>
                         {fontSizeList.map((item) => {
                             return (
                                 <option key={item.id} className='text-center fw-bolder' value={item.value}>{item.title}</option>
@@ -51,29 +50,27 @@ const Control = () => {
                 </InputGroup>
                 <InputGroup className='d-flex justify-center align-items-center'>
                     <Form.Label id='editor-tabwidth' className='mx-1 mb-0'>Tab Width : </Form.Label>
-                    <Form.Select size='sm' id='editor-tabwidth'>
+                    <Form.Select className='editor__control--select text-white' size='sm' id='editor-tabwidth' onChange={(e) => useStore.changeTabWidth(e.target.value)}>
                         {tabWidthList.map((item) => {
                             return (
                                 <option key={item.id} className='text-center fw-bolder' value={item.value}>{item.value}</option>
                             )
                         })}
                     </Form.Select>
-                    <Form.Select style={{marginTop: 5}} size='sm' id='editor-fontfamily'>
+                    <Form.Select className='editor__control--select text-white' style={{ marginTop: 15 }} size='sm' id='editor-fontfamily' onChange={(e) => useStore.changeFontFamily(e.target.value)}>
 
                         {fontFamilyList.map((item) => {
                             return (
                                 <option key={item.id} className='text-center fw-bolder' value={item.value}>{item.title}</option>
                             )
                         })}
-                        
                     </Form.Select>
                 </InputGroup>
                 <InputGroup className='mt-3'>
-                    <Form.Check color='orange' reverse label="Autosave" type="switch" id="editor-autosave" />
+                    <Form.Check color='secondary' defaultChecked reverse label="Autosave" type="switch" id="editor-autosave" />
                 </InputGroup>
             </fieldset>
         </Form>
-
     )
 }
 
