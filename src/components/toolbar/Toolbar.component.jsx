6487b429eac9d0d29666b0ec1aa060aecc6896c3
Foster Z <fosterunlimited0123@gmail.com>
2025-06-12 21:39:09 +0530

    feat(button): add button with running icon

--- src/components/toolbar/Toolbar.component.jsx
+++ src/components/toolbar/Toolbar.component.jsx
@@ -1,33 +1,43 @@
 // Zustand Imports
 import { store } from '../../store/store';
 
+// Bootstrap Imports
+import { Button } from 'react-bootstrap';
+
+// React Icons Imports
+import { FaRunning } from "react-icons/fa";
+
 // Stylesheet Imports
 import './toolbar.component.scss';
-import { useState } from 'react';
 
 // Root Component (Toolbar)
 const Toolbar = () => {
-    const useStore = store((prop) => prop)
+	const useStore = store((prop) => prop)
+	const languageList = [
+		{ id: 1, value: 'javascript', title: 'Javascript' },
+		{ id: 2, value: 'python', title: 'Python' },
+		{ id: 3, value: 'lua', title: 'Lua' },
+		{ id: 4, value: 'ruby', title: 'Ruby' }
+	]
 
-    const languageList = [
-        { id: 1, value: 'javascript', title: 'Javascript' },
-        { id: 2, value: 'python', title: 'Python' },
-        { id: 3, value: 'lua', title: 'Lua' },
-        { id: 4, value: 'ruby', title: 'Ruby' }
-    ]
-
-    return (
-        <div className="toolbar">
-            <div className='editor__main--language--selector'>
-                <select className='text-warning' onChange={(e) => useStore.changeLanguage(e.target.value)}>
-                    {languageList.map((item) => {
-                        return (
-                            <option key={item.id} value={item.value}>{item.title}</option>
-                        )
-                    })}
-                </select>
-            </div>
-        </div>
-    )
+	return (
+		<div className="toolbar">
+			<div className='editor__main--language--selector'>
+				<select className='text-warning' onChange={(e) => useStore.changeLanguage(e.target.value)}>
+					{languageList.map((item) => {
+						return (
+							<option key={item.id} value={item.value}>{item.title}</option>
+						)
+					})}
+				</select>
+			</div>
+			<div className='editor__main--language--control'>
+				<Button className='rounded-pill bg-success'>
+					<FaRunning size={20} />
+				</Button>
+			</div>
+		</div>
+	)
 }
+
 export default Toolbar;
