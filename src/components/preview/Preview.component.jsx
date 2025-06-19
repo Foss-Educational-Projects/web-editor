c4cbc3b11374a23150c6c47ba05dd05a7092264b
Foster Z <fosterunlimited0123@gmail.com>
2025-06-19 17:10:29 +0530

    feat(output): add editor output from the server to the preview component

--- src/components/preview/Preview.component.jsx
+++ src/components/preview/Preview.component.jsx
@@ -1,14 +1,18 @@
 // Custom User Imports
 import Toolbar from '../toolbar/Toolbar.component';
 
+// Import Zustand Store
+import { store } from '../../store/store';
+
 import './preview.component.scss';
 
 const Preview = () => {
+    const useStore = store((prop) => prop)
     return (
         <div className='container preview__main'>
             <Toolbar />
             <div className='container preview__main--output'>
-
+                {useStore.editorOutput}
             </div>
         </div>
     )
