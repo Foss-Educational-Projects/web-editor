5f3f344807ec2d01f2c48c587bbc66d6e4959579
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmail.com>
2025-11-15 20:06:45 +0530

    feat: add controls for the editor

--- src/components/control/Control.component.tsx
+++ src/components/control/Control.component.tsx
@@ -1,23 +1,30 @@
 // React Imports
 import { useState, useEffect } from 'react'
 
-// React Bootstrap Imports
-import { Form, InputGroup } from 'react-bootstrap'
 import { store } from '../../store/store'
+
+import { Select, Checkbox, Card } from 'antd';
+
+
 // Stylesheet Imports
 import './control.component.scss'
 
 // Root Component (Control)
 const Control = () => {
-    const useStore = store((state) => state)
+    const useStore: any = store((state) => state)
     const [font, setFont] = useState('')
 
-    const handleFontFamily = (e) => {
-        localStorage.setItem('font-family', e.target.value)
-        useStore.changeFontFamily(e.target.value)
+    const handleFontFamily = (e: any) => {
+        localStorage.setItem('font-family', e)
+        useStore.changeFontFamily(e)
+    }
+    const handleColorScheme = (e: any) => {
+        localStorage.setItem('colorscheme', e)
+        useStore.changeScheme(e)
+        console.log(e)
     }
     useEffect(
-        (e) => {
+        (e: any) => {
             const fontFamily = localStorage.getItem('font-family')
             setFont(fontFamily)
             if (fontFamily) {
@@ -26,13 +33,17 @@ const Control = () => {
         },
         [font]
     )
+    const ColorSchemes = [
+        { id: 1, label: 'GitHub Light', value: 'github' },
+        { id: 2, label: 'Monokai', value: 'monokai' },
+        { id: 3, label: 'Twilight', value: 'twilight' },
+        { id: 4, label: 'Gruvbox', value: 'gruvbox_dark_hard' },
+    ]
+
     const fontFamilyList = [
         { id: 1, title: 'Ubuntu Mono', value: 'UbuntuMono' },
-        { id: 2, title: 'Cascadia Code', value: 'CascadiaCode' },
-        { id: 3, title: 'Hack', value: 'Hack' },
         { id: 4, title: 'JetBrains Mono', value: 'JetBrainsMono' },
         { id: 5, title: 'Fira Code', value: 'FiraCode' },
-        { id: 6, title: 'DejaVu Sans', value: 'DejaVuSans' },
     ]
 
     const fontSizeList = [
@@ -44,116 +55,29 @@ const Control = () => {
     ]
 
     const tabWidthList = [
-        { id: 1, value: 2 },
-        { id: 2, value: 4 },
-        { id: 3, value: 8 },
+        { id: 1,label: '2', value: 2 },
+        { id: 2,label: '4', value: 4 },
+        { id: 3,label: '6', value: 8 },
     ]
 
     return (
-        <Form action="" className="editor__control--form">
-            <fieldset className="editor__control--form--settings">
-                <legend>Editor Settings</legend>
-                <Form.Check
-                    checked={useStore.snippets ? true : false}
-                    className="mb-3"
-                    type="checkbox"
-                    onChange={() => useStore.toggleSnippets()}
-                    label="Autocompletion"
+       <div>
+        <Card title="Editor Settings">
+            <Checkbox style={{marginBottom: '5px'}} checked>Enable Autocomplete</Checkbox>
+            <Checkbox style={{marginBottom: '5px'}}>Use Space</Checkbox>
+            <Checkbox style={{marginBottom: '10px'}} checked>Enable Autosave</Checkbox>
+            <Select placeholder='Font Size' options={fontSizeList} size='large' style={{width: '100%', marginBottom: '10px'}} />
+            <Select placeholder='Tab Width' options={tabWidthList} size='large' style={{width: '100%', marginBottom: '10px'}} />
+            <Select placeholder='Font Family' options={fontFamilyList} size='large' style={{width: '100%', marginBottom: '10px'}} />
+            <Select
+                    placeholder='Color Scheme'
+                    onChange={handleColorScheme}
+                    style={{width: '100%', marginBottom: '10px'}}
+                    options={ColorSchemes}
+                    size='large'
                 />
-                <Form.Check
-                    className="mb-3"
-                    type="checkbox"
-                    label="Use Spaces Instead of Tabs"
-                    checked={useStore.useSpace ? true : false}
-                    onChange={() => useStore.toggleSpacing()}
-                />
-                <InputGroup className="d-flex justify-center align-items-center mb-3">
-                    <Form.Label id="editor-fontsize" className="mx-1 mb-0">
-                        Font Size :{' '}
-                    </Form.Label>
-                    <Form.Select
-                        className="editor__control--select text-white"
-                        size="sm"
-                        id="editor-fontsize"
-                        defaultValue={useStore.fontSize}
-                        onChange={(e) =>
-                            useStore.changeFontSize(e.target.value)
-                        }
-                    >
-                        {fontSizeList.map((item) => {
-                            return (
-                                <option
-                                    key={item.id}
-                                    className="text-center fw-bolder"
-                                    value={item.value}
-                                >
-                                    {item.title}
-                                </option>
-                            )
-                        })}
-                    </Form.Select>
-                </InputGroup>
-                <InputGroup className="d-flex justify-center align-items-center">
-                    <Form.Label id="editor-tabwidth" className="mx-1 mb-0">
-                        Tab Width :{' '}
-                    </Form.Label>
-                    <Form.Select
-                        className="editor__control--select text-white"
-                        size="sm"
-                        id="editor-tabwidth"
-                        onChange={(e) =>
-                            useStore.changeTabWidth(e.target.value)
-                        }
-                    >
-                        {tabWidthList.map((item) => {
-                            return (
-                                <option
-                                    key={item.id}
-                                    className="text-center fw-bolder"
-                                    value={item.value}
-                                >
-                                    {item.value}
-                                </option>
-                            )
-                        })}
-                    </Form.Select>
-                </InputGroup>
-                <InputGroup>
-                    <Form.Select
-                        className="editor__control--select text-white"
-                        style={{ marginTop: 15 }}
-                        size="sm"
-                        id="editor-fontfamily"
-                        onChange={handleFontFamily}
-                    >
-                        {fontFamilyList.map((item) => {
-                            return (
-                                <option
-                                    selected={
-                                        item.value === font ? 'selected' : ''
-                                    }
-                                    key={item.id}
-                                    className="text-center fw-bolder"
-                                    value={item.value}
-                                >
-                                    {item.title}
-                                </option>
-                            )
-                        })}
-                    </Form.Select>
-                </InputGroup>
-                <InputGroup className="mt-3">
-                    <Form.Check
-                        color="secondary"
-                        defaultChecked
-                        reverse
-                        label="Autosave"
-                        type="switch"
-                        id="editor-autosave"
-                    />
-                </InputGroup>
-            </fieldset>
-        </Form>
+        </Card>
+       </div>
     )
 }
 
